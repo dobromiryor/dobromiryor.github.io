b07562d41e3db5da3f9596a6b924c823379e280d
Dobromir Yordanov <32433021+dobromiryor@users.noreply.github.com>
2024-05-23 17:23:10 +0300

    fix: 🐛 reset gravity on route change

--- src/hooks/useGravity.ts
+++ src/hooks/useGravity.ts
@@ -1,5 +1,7 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { useHashLocation } from "wouter/use-hash-location";
 
+import { Route } from "../enums/route.enum";
 import { GravityContext } from "../providers/gravity-provider";
 
 export const useGravity = () => {
@@ -8,6 +10,16 @@ export const useGravity = () => {
 	if (context === undefined) {
 		throw new Error("useGravity must be used within a GravityProvider");
 	}
+
+	const [_, setIsRunning] = context ?? [];
+
+	const [location] = useHashLocation();
+
+	useEffect(() => {
+		if (location !== Route.HOME) {
+			setIsRunning(false);
+		}
+	}, [location, setIsRunning]);
 
 	return context;
 };
